f995ffa0027811c5cb5d708996a93c0cc08c4410
olga.reznikova <olga.the.dark@gmail.com>
2012-06-08 10:58:15 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TarotOrganizer::Application.config.secret_token = 'c305eb91cee60783b1a35daae488b85988b77adaae2b5f7f86100efed0de2fc61a77049292a2abfdc46f6b5c7ea4e6e62026efa0b2f2bd853736eeda55488421'
