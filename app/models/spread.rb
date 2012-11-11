30cecb27edd11309229722f81bc7c316bca2ae09
olga.reznikova <olga.the.dark@gmail.com>
2012-11-11 02:55:08 -0800

    model callbacks bug fix

--- app/models/spread.rb
+++ app/models/spread.rb
@@ -2,12 +2,14 @@ class Spread < ActiveRecord::Base
   require 'json'
   belongs_to :client
   validates_with SpreadValidator
-  before_save :generate_image
-  
+  after_save :generate_image
+
   def generate_image
     image = SpreadImage.new(self)
     image.render
-    self.image = image.path
+    Spread.skip_callback(:save, :after, :generate_image)
+    self.update_column(:image, image.path)
+    Spread.set_callback(:save, :after, :generate_image)
   end
-  
+
 end
