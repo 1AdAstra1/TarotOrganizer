f417faebb9ce63ab8cd118d9adeb47210cfd8bfc
olga.reznikova <olga.the.dark@gmail.com>
2012-11-13 11:09:26 -0800

    Devise views customization

--- features/step_definitions/client_steps.rb
+++ features/step_definitions/client_steps.rb
@@ -47,7 +47,7 @@ Given /I am logged in as (.*)/ do |user_email|
   visit('/users/sign_in');
   fill_in("user_email", :with => user[:email])
   fill_in("user_password", :with =>user[:password])
-  click_button("Sign in")
+  click_button("login_button")
 end
 
 def group_exists(unsee, list, xpath='//*')
