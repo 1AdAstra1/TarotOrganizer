9c6dd9119d7e5276f1e50a0461d9911f16a999a6
olga.reznikova <olga.the.dark@gmail.com>
2012-11-15 11:13:46 -0800

    HTML code export

---
+++ app/assets/javascripts/spread_html.js
@@ -0,0 +1,30 @@
+var getSpreadHTML = function(imageUrl) {
+	var wrapper = $('<div></div>'),
+    innerWrapper = $('<div></div>').appendTo(wrapper),
+    spreadImage = $('<img />').attr({
+		'src' : imageUrl
+    }),
+    descriptionsExport = $('#descriptions').clone();
+    innerWrapper.append(spreadImage);
+    descriptionsExport.find('span').removeClass();
+    descriptionsExport.appendTo(wrapper);
+
+    return wrapper.html();
+};
+
+var loadExport = function(mode, imageUrl) {
+	var content,
+	exportBox = $('#export'),
+	exportArea = $('<textarea></textarea>');
+	switch(mode) {
+		case 'html':
+			content = getSpreadHTML(imageUrl);
+		break;
+		case 'bbcode':
+			content = '';
+		break;
+	}
+	exportBox.html('');
+	exportArea.text(content).appendTo(exportBox);
+			
+};
