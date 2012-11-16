a2d50883c496e8a655595dd71c67137b94fa6561
olga.reznikova <olga.the.dark@gmail.com>
2012-11-16 08:50:48 -0800

    BBCode export

--- app/assets/javascripts/spread_html.js
+++ app/assets/javascripts/spread_html.js
@@ -4,13 +4,22 @@ var getSpreadHTML = function(imageUrl) {
     spreadImage = $('<img />').attr({
 		'src' : imageUrl
     }),
-    descriptionsExport = $('#descriptions').clone();
+    descriptionsExport = $('#descriptions').clone().removeAttr('id');
     innerWrapper.append(spreadImage);
-    descriptionsExport.find('span').removeClass();
+    descriptionsExport.find('span').removeAttr('class');
     descriptionsExport.appendTo(wrapper);
 
     return wrapper.html().replace(/(\r\n|\n|\r)/gm, "");
 };
+
+var getBBCode = function(imageUrl) {
+	var source_html = getSpreadHTML(imageUrl).replace(/<(\/)?div>/gm, "").replace(/<(\/)?span>/gm, ""),
+	bbcode = source_html.replace(/<img src="([^"]+)"\s*(\/)?>/, "[img]$1[/img]")
+						.replace(/<ol>/, "[list=1]").replace(/<\/ol>/, "[/list]")
+						.replace(/<(\/)?strong>/gm, "[$1b]")
+						.replace(/<li>([^<]+)<\/li>/gm, "[*] $1");
+	return bbcode;	
+}
 
 var loadExport = function(mode, imageUrl) {
 	var content,
@@ -21,7 +30,7 @@ var loadExport = function(mode, imageUrl) {
 			content = getSpreadHTML(imageUrl);
 		break;
 		case 'bbcode':
-			content = '';
+			content = getBBCode(imageUrl);
 		break;
 	}
 	exportBox.html('');
