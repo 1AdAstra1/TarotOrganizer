7c197da60853b0cb11f37719527c8a32d141a84b
olga.reznikova <olga.the.dark@gmail.com>
2012-12-17 10:58:25 -0800

    copyright link

--- app/assets/javascripts/spread_html.js
+++ app/assets/javascripts/spread_html.js
@@ -1,23 +1,34 @@
-var getSpreadHTML = function(imageUrl) {
-	var wrapper = $('<div></div>'),
-    innerWrapper = $('<div></div>').appendTo(wrapper),
-    spreadImage = $('<img />').attr({
+var getExportDOM = function(imageUrl) {
+	this.wrapper = $('<div></div>');
+    this.innerWrapper = $('<div></div>').appendTo(this.wrapper);
+    this.spreadImage = $('<img />').attr({
 		'src' : imageUrl
-    }),
-    descriptionsExport = $('#descriptions').clone().removeAttr('id').removeAttr('class');
-    innerWrapper.append(spreadImage);
-    descriptionsExport.find('span').removeAttr('class');
-    descriptionsExport.appendTo(wrapper);
+    }).appendTo(this.innerWrapper);
+    this.descriptionsExport = $('#descriptions').clone().removeAttr('id').removeAttr('class').appendTo(this.wrapper);
+    this.descriptionsExport.find('span').removeAttr('class');
+	this.copyright = $('<div style="text-align:right">Расклад сделан в <a href="http://tarot-organizer.heroku.com">Блокноте Таролога</a></div>').appendTo(this.wrapper);
+    this.getHTML = function() {
+    	return this.wrapper.html().replace(/(\r\n|\n|\r)/gm, "").replace(/([ \t])[ \t]+/gm, "$1");
+    };
+    return this;
+};
 
-    return wrapper.html().replace(/(\r\n|\n|\r)/gm, "").replace(/([ \t])[ \t]+/gm, "$1");
+var getSpreadHTML = function(imageUrl) {
+	this.dom = new getExportDOM(imageUrl);
+    return this.dom.getHTML();
 };
 
 var getBBCode = function(imageUrl) {
-	var source_html = getSpreadHTML(imageUrl).replace(/<(\/)?div>/gm, "").replace(/<(\/)?span>/gm, ""),
+	this.dom = new getExportDOM(imageUrl);
+	console.log(this.dom.copyright);
+	this.dom.copyright.remove();
+	var source_html = this.dom.getHTML().replace(/<(\/)?div>/gm, "").replace(/<(\/)?span>/gm, ""),
 	bbcode = source_html.replace(/<img src="([^"]+)"\s*(\/)?>/, "[img]$1[/img]")
 						.replace(/<ol>/, "[list=1]").replace(/<\/ol>/, "[/list]")
 						.replace(/<(\/)?strong>/gm, "[$1b]")
 						.replace(/<li>([^<]+)<\/li>/gm, "[*]$1[/*]");
+	bbcode += "[right]Расклад сделан в [url=http://tarot-organizer.heroku.com]Блокноте Таролога[/url]﻿[/right]"
+						
 	return bbcode;	
 }
 
