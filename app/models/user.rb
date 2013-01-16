109470332febeaa584deb8f8e6cb5a2c9f64b6fe
olga.reznikova <olga.the.dark@gmail.com>
2013-01-16 11:55:27 -0800

    user roles in database

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   has_many :clients, :dependent => :destroy
   has_many :spreads, :dependent => :destroy
+  ROLES = %w[admin paid_user user]
+  
+  def method_missing(method_name, *args)
+    case method_name
+     when /^is_(\w+)?/ then
+      role == $1
+    else
+      raise
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130110191346) do
+ActiveRecord::Schema.define(:version => 20130116194426) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(:version => 20130110191346) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
